513113f6cdce224deb2f699bca004ebcf4b93bc8
LuYF-Lemon-love <3555028709@qq.com>
2022-07-20 12:34:00 +0800

    Site updated: 2022-07-20 12:33:30

---
+++ js/butterflycirclemagic.js
@@ -0,0 +1,9 @@
+// butterfly 3.6.0-b5头图气泡
+(function () {
+  $("#page-header").circleMagic({
+    radius: 35,
+    density: 0.1,
+    color: "rgba(255,255,255, .4)",
+    clearOffset: 0.9,
+  });
+})();
